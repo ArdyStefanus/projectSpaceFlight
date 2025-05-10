6f30a78bdccbbfc7aa1116837a8651d986d50f60
ArdyStefanus <stefanusardy97@gmail.com>
2025-05-10 09:12:50 +0700

    add class tab detail list vc
    Signed-off-by: ArdyStefanus <stefanusardy97@gmail.com>

---
+++ ProjectSpaceFlight/TabBerandaVC/Cell/BerandaCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  BerandaCollectionCell.swift
+//  ProjectSpaceFlight
+//
+//  Created by Macbook Pro on 10/05/25.
+//
+
+import UIKit
+
+class BerandaCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ProjectSpaceFlight/TabDetailListVC/TabDetailListVC.swift
@@ -0,0 +1,16 @@
+//
+//  TabDetailListVC.swift
+//  ProjectSpaceFlight
+//
+//  Created by Macbook Pro on 10/05/25.
+//
+
+import UIKit
+
+class TabDetailListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
